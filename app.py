ada6021bcd720165d131cc2ceb337ec1c723bead
s22615 <s22615@pjwstk.edu.pl>
2021-06-08 14:30:51 +0200

    update to login functionality

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
-
-app = Flask(__name__)
+from flask import Flask, render_template, request, redirect, url_for, session
 
 
 class User:
@@ -14,17 +12,32 @@ class User:
 
 
 users = []
-users.append(User(id=1,username='Sebastian',password='123'))
+users.append(User(id=1, username='Sebastian', password='123'))
+
+app = Flask(__name__)
+app.secret_key = 'wejoaiw29134b21oeaw'
 
 
-@app.route('/')
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        session.pop('user_id', None)
+
+        username = request.form['username']
+        password = request.form['password']
+
+        user = [x for x in users if x.username == username][0]
+        if user and user.password == password:
+            session['user_id'] = user.id
+            return redirect(url_for('home'))
+
+        return redirect(url_for('login'))
+
     return render_template('login.html')
 
 
 @app.route('/home')
-def login():
+def home():
     return render_template('homePage.html')
 
 
