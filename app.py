f5222283290ac823a4eae3dcf7fa698450a18cc8
s22615 <s22615@pjwstk.edu.pl>
2021-06-15 19:54:53 +0200

    problem with sending data from register to database - solved

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import sqlite3
 from flask import Flask, render_template, request, redirect, url_for, session, g
 
 app = Flask(__name__)
+
 app.secret_key = 'wejoaiw29134b21oeaw'
 
 DATABASE = 'C:/Users/Sebastian/PycharmProjects/ProjektWPRG/komisdb.sqlite'
@@ -50,8 +51,28 @@ def login():
     return render_template('login.html')
 
 
-@app.route('/register')
+@app.route('/register', methods=('GET', 'POST'))
 def register():
+    if request.method == 'POST':
+        print(request.form)
+        loginR = request.form['loginr']
+        passR = request.form['passwordr']
+        nameR = request.form['name']
+        surnameR = request.form['surname']
+        peselR = request.form['pesel']
+        phoneR = request.form['phone']
+        dateR = request.form['date']
+        postcodeR = request.form['postcode']
+        cityR = request.form['city']
+        streetR = request.form['street']
+
+        database = get_db()
+        adresIn = database.execute("INSERT INTO adres (kod_pocztowy,miasto,ulica) VALUES (?,?,?)", (postcodeR,cityR,streetR))
+        klientIn = database.execute("INSERT INTO klient (imie,nazwisko,pesel,nr_tel,data_urodzenia,login,haslo,adres) VALUES (?,?,?,?,?,?,?,?)", (nameR, surnameR,peselR,phoneR,dateR,loginR,passR,adresIn.lastrowid))
+        database.commit()
+        adresIn.close()
+        klientIn.close()
+        return redirect(url_for('login'))
     return render_template('register.html')
 
 
@@ -64,14 +85,29 @@ def logout():
 
 @app.route('/createoffer')
 def createoffer():
+    if request.method == 'POST':
+        nrrej = request.method['nrrej']
+        marka = request.method['marka']
+        model = request.method['model']
+        rocznik = request.method['rocznik']
+        przebieg = request.method['przebieg']
+        pojs = request.method['pojs']
+        kolor = request.method['kolor']
+        cena = request.method['cena']
+
+        database = get_db()
+        samochodIn = database.executescript("INSERT INTO samochod (nr_rejestracyjny,marka,model,rocznik,przebieg,pojemnosc_skokowa,kolor,id_wlasciciela) VALUES (?,?,?,?,?,?,?,?)", (nrrej,marka,model,rocznik,przebieg,pojs,kolor,))
+        database.commit()
+        samochodIn.close()
+        return redirect(url_for('home'))
     return render_template('createoffer.html')
 
 
 @app.route('/home')
 def home():
-    offer = query_db("SELECT * FROM oferta")
+    offer = query_db("SELECT * from oferta inner join samochod s on s.nr_rejestracyjny = oferta.nr_rejestracyjny join klient k on k.id_klienta = s.id_wlasciciela")
     return render_template('homePage.html', offer=offer, username=session['username'])
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
