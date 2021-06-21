f8acefba6d6f1776173c0df78dbc0645fd9641e3
s22615 <s22615@pjwstk.edu.pl>
2021-06-21 19:39:13 +0200

    problem with editting and searching and offers - solved

--- app.py
+++ app.py
@@ -80,7 +80,7 @@ def register():
     return render_template('register.html')
 
 
-@app.route('/createoffer', methods=('GET','POST'))
+@app.route('/createoffer', methods=('GET', 'POST'))
 def createoffer():
     username = session['username']
     if request.method == 'POST':
@@ -101,12 +101,64 @@ def createoffer():
             (nrrej, marka, model, rocznik, przebieg, pojs, kolor, selecteduserId))
         selectcarId = query_db("SELECT id_samochodu FROM samochod WHERE nr_rejestracyjny=?", (nrrej,), True)
         selectedcarId = selectcarId['id_samochodu']
-        ofertaIn = database.execute("INSERT INTO oferta(cena,id_samochodu) VALUES (?,?)",(cena,selectedcarId))
+        ofertaIn = database.execute("INSERT INTO oferta(cena,id_samochodu) VALUES (?,?)", (cena, selectedcarId))
         database.commit()
         samochodIn.close()
         ofertaIn.close()
         return redirect(url_for('home'))
     return render_template('createoffer.html')
+
+
+@app.route('/controlsite', methods=('GET', 'POST'))
+def controlsite():
+    username = session['username']
+    database = get_db()
+    selectUser = query_db("SELECT id_klienta FROM klient WHERE login= ?", (username,), True)
+    selecteduserId = selectUser['id_klienta']
+    offer = query_db(
+        "SELECT * from oferta inner join samochod s on s.id_samochodu = oferta.id_samochodu join klient k on k.id_klienta = s.id_wlasciciela WHERE id_klienta = ?",
+        (selecteduserId,))
+    database.commit()
+    return render_template('controlSite.html', offer=offer, username=session['username'])
+
+
+@app.route('/deleteoffer', methods=("POST",))
+def deleteoffer():
+    id_oferty = request.form['id_oferty']
+    print(id_oferty)
+    database = get_db()
+    deleteselectedOffer = database.execute("DELETE FROM oferta WHERE id_oferty=?", (id_oferty,))
+    database.commit()
+    deleteselectedOffer.close()
+    return redirect(url_for('controlsite'))
+
+
+@app.route('/editoffer/<int:id_oferty>', methods=("GET", "POST"))
+def editoffer(id_oferty):
+    if request.method == 'GET':
+        offer = query_db(
+            "SELECT * from oferta inner join samochod s on s.id_samochodu = oferta.id_samochodu join klient k on k.id_klienta = s.id_wlasciciela WHERE id_oferty=?",
+            (id_oferty,))
+        return render_template('editOffer.html', each=offer[0])
+    elif request.method == 'POST':
+        nrrej = request.form['nrrej']
+        marka = request.form['marka']
+        model = request.form['model']
+        rocznik = request.form['rocznik']
+        przebieg = request.form['przebieg']
+        pojs = request.form['pojs']
+        kolor = request.form['kolor']
+        cena = request.form['cena']
+        id_klienta = request.form['idklienta']
+        database = get_db()
+        updatecar = database.execute(
+            "UPDATE samochod SET nr_rejestracyjny = ?,marka = ?,model = ?,rocznik = ?,przebieg = ?,pojemnosc_skokowa = ?,kolor=? WHERE id_wlasciciela=?",
+            (nrrej, marka, model, rocznik, przebieg, pojs, kolor, id_klienta))
+        updateoffer = database.execute("UPDATE oferta SET cena=? WHERE id_oferty=?", (cena, id_oferty))
+        database.commit()
+        updatecar.close()
+        updateoffer.close()
+        return redirect(url_for('controlsite'))
 
 
 @app.route('/logout')
@@ -120,6 +172,16 @@ def logout():
 def home():
     offer = query_db(
         "SELECT * from oferta inner join samochod s on s.id_samochodu = oferta.id_samochodu join klient k on k.id_klienta = s.id_wlasciciela")
+    return render_template('homePage.html', offer=offer, username=session['username'])
+
+
+@app.route('/home/search', methods=("POST",))
+def homesearch():
+    modelb = request.form['modelb']
+    offer = query_db("""SELECT * FROM samochod 
+    JOIN oferta ON samochod.id_samochodu=oferta.id_samochodu 
+    JOIN klient ON samochod.id_wlasciciela = klient.id_klienta 
+    WHERE model LIKE ?""",("%"+modelb+"%",))
     return render_template('homePage.html', offer=offer, username=session['username'])
 
 
